3f90ee3294d4d4c240018e2cb21737643cb865a6
gkirov <kirov.a.georgi@gmail.com>
2020-07-22 16:37:31 +0300

    First commit

---
+++ src/app/services/data.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Observable, BehaviorSubject, Subject } from 'rxjs';
+import { HttpClient } from 'selenium-webdriver/http';
+import { Company, Employee, Project } from './dataModels';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  private companies$: Subject<Company[]> = new BehaviorSubject([]);
+  private employees$: Subject<Employee[]> = new BehaviorSubject([]);
+  private projects$: Subject<Project[]> = new BehaviorSubject([]);
+
+  constructor(private http: HttpClient) { }
+
+  getCompanies(): Observable<any> {
+    return this.companies$;
+  }
+
+  getEmployees(): Observable<any> {
+    return this.employees$;
+  }
+
+  getProjects(): Observable<any> {
+    return this.projects$;
+  }
+}
---
+++ src/app/services/dataModels.ts
@@ -0,0 +1,42 @@
+export class Company {
+  constructor(
+    public id: string,
+    public name: string,
+    public business: string,
+    public slogan: string,
+  ) { }
+}
+
+export class Employee {
+  constructor(
+    public id: string,
+    public firstName: string,
+    public lastName: string,
+    public dateOfBirth: Date,
+    public companyId: string,
+    public jobTitle: string,
+    public jobArea: string,
+    public jobType: string,
+  ) { }
+}
+
+export class Project {
+  constructor(
+    public id: string,
+    public name: string,
+    public department: string,
+    public employeesId: string[],
+    public companyId: string,
+  ) { }
+}
+
+export class CompanyAddress {
+  constructor(
+    public id: string,
+    public city: string,
+    public country: string,
+    public street: string,
+    public state: string,
+    public companyId: string,
+  ) { }
+}
