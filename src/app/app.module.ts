b909c242107b0a260830630a9369c9b6be352f86
gkirov <kirov.a.georgi@gmail.com>
2020-07-24 01:31:23 +0300

    Add Navigation and create menu data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { CompanyComponent } from './components/company/company/company.component';
+import { NavigationComponent } from './components/navigation/navigation/navigation.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// *** PrimeNg ***
+import { TieredMenuModule } from 'primeng/tieredmenu';
+// *** PrimeNg ***
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompanyComponent,
+    NavigationComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    BrowserAnimationsModule,
+    TieredMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
